e3d6538b37fb1c5815ad7fef83c13f320eea5223
Konrad Jasman <kjasman@uvic.ca>
2024-11-02 14:46:09 -0700

    revamped patient queue

--- app/backend/patient_queue.py
+++ app/backend/patient_queue.py
@@ -3,35 +3,83 @@
 # - add
 # - remove
 # - send update to observers (all patients on queue)
-class Queue:
+from people import Patient
+from typing import List
+import csv
+
+
+class PatientQueue:
     def __init__(self):
         self.queue = []
         self.observers = []
 
-    # Add a patient to the queue
     def add(self, patient):
-        self.queue.append(patient)  # TODO: Insert patient into queue based on triage score
+        """
+        Add a patient to the queue based on triage score and priority.
+        
+        Args:
+            patient: Patient object to be added
+            
+        Returns:
+            int: Position where patient was inserted
+        """
+        # Find insert position based on triage score and priority
+        insert_position = 0
+        for i, existing_patient in enumerate(self.queue):
+            # Critical patients go first
+            if patient.priority == "critical" and existing_patient.priority != "critical":
+                break
+            # Then sort by triage score
+            elif patient.triage_score <= existing_patient.triage_score:
+                insert_position = i + 1
+            else:
+                break
+                
+        self.queue.insert(insert_position, patient)
+        self.add_observer(patient)  # Automatically add patient as observer
         self.update_queue_positions()
         self.notify_observers()
-        # Return index of patient in the queue
-        return len(self.queue) - 1
+        return insert_position
 
-    # Remove a patient from the queue
     def remove(self, patient):
-        self.queue.remove(patient)
-        self.update_queue_positions()
-        self.notify_observers()
+        """
+        Remove a patient from the queue
+        
+        Args:
+            patient: Patient object to be removed
+        """
+        if patient in self.queue:
+            self.queue.remove(patient)
+            self.remove_observer(patient)  # Remove patient from observers
+            self.update_queue_positions()
+            self.notify_observers()
 
-    # Add an observer to the queue
     def add_observer(self, observer):
-        self.observers.append(observer)
+        """
+        Add a patient as an observer
+        
+        Args:
+            observer: Patient object to observe queue
+        """
+        if observer not in self.observers:
+            self.observers.append(observer)
 
-    # Notify all observers of a change in the queue
+    def remove_observer(self, observer):
+        """
+        Remove a patient from observers
+        
+        Args:
+            observer: Patient object to remove from observers
+        """
+        if observer in self.observers:
+            self.observers.remove(observer)
+
     def notify_observers(self):
+        """Notify all observers of queue changes"""
         for observer in self.observers:
             observer.update(self.queue)
 
-    # Update queue positions for all patients: observer pattern
     def update_queue_positions(self):
+        """Update positions for all patients in queue"""
         for index, patient in enumerate(self.queue):
             patient.modify_q_pos(index)
